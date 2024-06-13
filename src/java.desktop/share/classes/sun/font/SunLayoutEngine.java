JetBrains/JetBrainsRuntime

Emoji PR sync (experimental)
- Removed font fallback hacks.
- Changed composite font glyph code encoding scheme, refactored to use Font2D instead of PhysicalFont.
- New Emoji logical font type with 2 underlying physical fonts.
- Removed getGlyphVectorOutline - it's unused and broken.
- Got rid of charsToGlyphs[NS] boilerplate.
- Moved emoji tests to separate directory.

src/java.desktop/share/classes/sun/font/SunLayoutEngine.java
@@ -164,7 +164,7 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
         return ref.getNativePtr();
     }
 
-    public void layout(FontStrikeDesc desc, float[] mat, float ptSize, int gmask,
+    public void layout(FontStrikeDesc desc, float[] mat, float ptSize, int slot, int slotShift,
                        int baseIndex, TextRecord tr, boolean ltrDirection, Map<String, Integer> features,
                        Point2D.Float pt, GVData data) {
         Font2D font = key.font();
@@ -174,7 +174,8 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
             shape(font, strike, ptSize, mat, pFace,
                     tr.text, data, key.script(),
                     tr.start, tr.limit, baseIndex, pt,
-                    ltrDirection, FontExtensions.featuresToStringArray(features), gmask);
+                    ltrDirection, FontExtensions.featuresToStringArray(features),
+                    slot, slotShift);
         }
     }
 
@@ -184,7 +185,8 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
               long pFace,
               char[] chars, GVData data,
               int script, int offset, int limit,
-              int baseIndex, Point2D.Float pt, boolean ltrDirection, String[] features, int slot);
+              int baseIndex, Point2D.Float pt, boolean ltrDirection, String[] features,
+              int slot, int slotShift);
 
     private static native long createFace(Font2D font,
                                           long platformNativeFontPtr);

oss-evaluation-repository/JetBrains-JetBrainsRuntime

src/java.desktop/share/classes/sun/font/SunLayoutEngine.java
@@ -177,7 +177,7 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
 
     }
 
-    public void layout(FontStrikeDesc desc, float[] mat, float ptSize, int gmask,
+    public void layout(FontStrikeDesc desc, float[] mat, float ptSize, int slot, int slotShift,
                        int baseIndex, TextRecord tr, boolean ltrDirection, Map<String, Integer> features,
                        Point2D.Float pt, GVData data) {
 
@@ -189,7 +189,8 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
                 HBShaper.shape(font, strike, ptSize, mat, face,
                         tr.text, data, key.script(),
                         tr.start, tr.limit, baseIndex, pt,
-                        ltrDirection, FontExtensions.featuresToString(features), gmask);
+                        ltrDirection, FontExtensions.featuresToString(features),
+                        slot, slotShift);
             }
         } else {
             long pFace = getFacePtr(font);
@@ -197,7 +198,8 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
                 SunLayoutEngine.shape(font, strike, ptSize, mat, pFace,
                         tr.text, data, key.script(),
                         tr.start, tr.limit, baseIndex, pt,
-                        ltrDirection, FontExtensions.featuresToString(features), gmask);
+                        ltrDirection, FontExtensions.featuresToString(features),
+                        slot, slotShift);
             }
         }
     }
@@ -208,7 +210,8 @@ public final class SunLayoutEngine implements LayoutEngine, LayoutEngineFactory 
               long pFace,
               char[] chars, GVData data,
               int script, int offset, int limit,
-              int baseIndex, Point2D.Float pt, boolean ltrDirection, String features, int slot);
+              int baseIndex, Point2D.Float pt, boolean ltrDirection, String features,
+              int slot, int slotShift);
 
     private static native long createFace(Font2D font,
                                           long platformNativeFontPtr);